9b0519838efdb5114e1e128c6197e848aec3828b
Ashkan Ashtiani <ashcan@3dln.com>
2021-12-20 07:22:04 +0330

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+import "reveal.js/dist/reveal.css";
+import "reveal.js/dist/theme/sky.css";
+// import "./style.css";
+import "./tailwind.css";
+
+import Reveal from "reveal.js";
+
+const deck = new Reveal();
+deck.initialize({ hash: true, slideNumber: true });

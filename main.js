dfef4b800554a28b100574465e82be359b7adc58
Ashkan Ashtiani <ashcan@3dln.com>
2021-12-20 12:24:52 +0330

    basic slides

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-
 import "reveal.js/dist/reset.css";
 import "reveal.js/dist/reveal.css";
-import "./sky.css";
+// import "./sky.css";
+
 import "./tailwind.css";
 
 import Reveal from "reveal.js";
 
 const deck = new Reveal();
-deck.initialize({ hash: true, slideNumber: true });
+deck.initialize({
+  hash: true,
+  slideNumber: true,
+  controls: true,
+  progress: true,
+  center: true,
+});
